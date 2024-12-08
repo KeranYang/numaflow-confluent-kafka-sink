54ba73344910b5e6149fbd9bf9eb2e25d9883c11
Keran Yang <yangkr920208@gmail.com>
2024-12-08 09:21:10 -0500

    chore: a working log sink
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main/java/io/numaproj/confluent/kafka_sink/KafkaSinkApplication.java
+++ src/main/java/io/numaproj/confluent/kafka_sink/KafkaSinkApplication.java
@@ -1,16 +1,55 @@
 package io.numaproj.confluent.kafka_sink;
 
+import io.numaproj.numaflow.sinker.Datum;
+import io.numaproj.numaflow.sinker.DatumIterator;
+import io.numaproj.numaflow.sinker.Response;
+import io.numaproj.numaflow.sinker.ResponseList;
+import io.numaproj.numaflow.sinker.Server;
+import io.numaproj.numaflow.sinker.Sinker;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @Slf4j
-public class KafkaSinkApplication {
+@SpringBootApplication
+public class KafkaSinkApplication extends Sinker {
 
-	public static void main(String[] args) {
-		log.info("Starting Confluent Kafka sink application...");
-		SpringApplication.run(KafkaSinkApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(new KafkaSinkApplication());
+
+		// Start the server
+		server.start();
+
+		// wait for the server to shut down
+		server.awaitTermination();
 	}
 
+	@Override
+	public ResponseList processMessages(DatumIterator datumIterator) {
+		ResponseList.ResponseListBuilder responseListBuilder = ResponseList.newBuilder();
+		while (true) {
+			Datum datum;
+			try {
+				datum = datumIterator.next();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				continue;
+			}
+			// null means the iterator is closed, so we break the loop
+			if (datum == null) {
+				break;
+			}
+			try {
+				String msg = new String(datum.getValue());
+				log.info("Received message: {}, headers - {}", msg, datum.getHeaders());
+				responseListBuilder.addResponse(Response.responseOK(datum.getId()));
+			} catch (Exception e) {
+				responseListBuilder.addResponse(Response.responseFailure(
+						datum.getId(),
+						e.getMessage()));
+			}
+		}
+		return responseListBuilder.build();
+	}
 }
